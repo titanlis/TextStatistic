a9e2cf69cc7d85cc2323f8ef51a80c9346382f7d
Andrei <33245539+titanlis@users.noreply.github.com>
2021-04-08 23:11:07 +0700

    Start analysis

--- src/main/java/MainwindowController.java
+++ src/main/java/MainwindowController.java
@@ -1,5 +1,7 @@
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 
 public class MainwindowController {
@@ -10,13 +12,26 @@ public class MainwindowController {
     Button okButton;
 
     @FXML
+    TextArea startText;
+
+    @FXML
+    Label textSize;
+
+
+    @FXML
     private void clickOkButton(){
-        System.out.println("Ok");
+        String sText = startText.getText();
+        textSize.setText( "Text size = " + sText.length() + " symbols.\n");
     }
 
     @FXML
     private void clickCancelButton(){
         ((Stage) cancelButton.getScene().getWindow()).close();
+    }
+
+    @FXML
+    public void initialize() {
+
     }
 }
 
